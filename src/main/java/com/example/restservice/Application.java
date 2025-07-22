fe4dfe00401e95bbcc1f6701fe48a9559b703592
Em S <eshockley@microsoft.com>
2025-07-22 14:37:33 -0700

    update

--- src/main/java/com/example/restservice/Application.java
+++ src/main/java/com/example/restservice/Application.java
@@ -2,11 +2,16 @@ package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+
+        // Attach Application Insights to the application
+        ApplicationInsights.attach();
+
         SpringApplication.run(Application.class, args);
     }
 
--- src/main/java/com/example/restservice/greeting/Greeting.java
+++ src/main/java/com/example/restservice/greeting/Greeting.java
@@ -17,4 +17,12 @@ public class Greeting {
 	public String getContent() {
 		return content;
 	}
+
+	@Override
+	public String toString() {
+		return "Greeting{" +
+				"id=" + id +
+				", content='" + content + '\'' +
+				'}';
+	}
 }
