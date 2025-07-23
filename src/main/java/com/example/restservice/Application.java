3984d8cc23c58e52e306fc25704398d5a88a8975
Em S <eshockley@microsoft.com>
2025-07-22 18:59:14 -0700

    app insight working now

--- src/main/java/com/example/restservice/Application.java
+++ src/main/java/com/example/restservice/Application.java
@@ -10,6 +10,10 @@ public class Application {
     public static void main(String[] args) {
 
         // Attach Application Insights to the application
+       //   System.setProperty("applicationinsights.runtime-attach.configuration.classpath.file", "src\\main\\resources\\applicationinsights.json");     
+
+       
+
         ApplicationInsights.attach();
 
         SpringApplication.run(Application.class, args);
